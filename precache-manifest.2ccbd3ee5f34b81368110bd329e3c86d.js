478b51e61542159cd229d9942011d3b23c1844ad
PiIsFour <PiIsFour@users.noreply.github.com>
2020-03-07 17:02:14 +0100

    add homepage base path

---
+++ precache-manifest.2ccbd3ee5f34b81368110bd329e3c86d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93590e857cd681aad436453c9f576808",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "75cb08216208efc4e00c",
+    "url": "/todo-list-react/static/css/main.4f5c16cb.chunk.css"
+  },
+  {
+    "revision": "0caf57cbdd35f82d3cb2",
+    "url": "/todo-list-react/static/js/2.02a9e7bd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todo-list-react/static/js/2.02a9e7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75cb08216208efc4e00c",
+    "url": "/todo-list-react/static/js/main.4f4d4ce6.chunk.js"
+  },
+  {
+    "revision": "b71fcba6614d9c37ef2b",
+    "url": "/todo-list-react/static/js/runtime-main.07b85b22.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e810f5942cfdd4b08701361d31ce136.js"
+  "/todo-list-react/precache-manifest.2ccbd3ee5f34b81368110bd329e3c86d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
